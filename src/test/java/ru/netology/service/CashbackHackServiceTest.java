8d30538d30213fadde1c183079c46b4ef97f2cfa
sibkas <106993347+sibkas@users.noreply.github.com>
2025-09-17 16:51:07 +0700

    Update CashbackHackServiceTest.java in junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,16 +6,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldReturnRemainingAmount() {
+    public void shouldReturnRemainingAmountWhenLessThanBoundary() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(900);
         assertEquals(100, actual);
     }
 
     @Test
-    public void shouldReturnZeroIfAmountIsExactMultiple() {
+    public void shouldReturnZeroWhenAmountIsExactlyBoundary() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
         assertEquals(0, actual);
+    }
+
+    @Test
+    public void shouldReturnRemainingAmountWhenMoreThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1150);
+        assertEquals(850, actual);
+    }
+
+    @Test
+    public void shouldReturnFullBoundaryWhenAmountIsZero() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(0);
+        assertEquals(1000, actual);
     }
 }
