797b32644594f223fc7f51d8029ad38344773df2
sibkas <106993347+sibkas@users.noreply.github.com>
2025-09-17 16:52:39 +0700

    Update CashbackHackServiceTest.java in testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,10 +6,30 @@ import org.testng.Assert;
 public class CashbackHackServiceTest {
 
     @Test
-    public void shouldReturnRemainingAmount() {
+    public void shouldReturnRemainingAmountWhenLessThanBoundary() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(900);
-        Assert.assertEquals(actual, 100); // actual, expected в TestNG
+        Assert.assertEquals(actual, 100); 
     }
 
+    @Test
+    public void shouldReturnZeroWhenAmountIsExactlyBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        Assert.assertEquals(actual, 0);
+    }
+
+    @Test
+    public void shouldReturnRemainingAmountWhenMoreThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1150);
+        Assert.assertEquals(actual, 850);
+    }
+
+    @Test
+    public void shouldReturnFullBoundaryWhenAmountIsZero() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(0);
+        Assert.assertEquals(actual, 1000);
+    }
 }
